26177eb5177e32d01129bc152acf1a51acce0175
Blair Felker <blairf@silber-courses.cis.udel.edu>
2022-09-23 20:57:24 +0000

    First plan commented in main.

--- MainShell.c
+++ MainShell.c
@@ -3,5 +3,15 @@
 
 int main(){
     printf("My name is Walter Hartwell  White");
+    //This loop prints a prompt (prefix).
+    //Initially a blank space followed by[cwd]>
+    //cwd is the current working directory
+    //This will execute commands until exit is entered
+    //
+    //The loop will check if a command is a built in command.
+    //If it is not, the [command]: Command not found. is printed.
+
+
+    //Each command supports arguments using an argv array.
     return 0;
 }
