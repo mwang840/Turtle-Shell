d7f7f7b691a1e9a8619e9ca18a7a8d8f327c35ae
mwang840 <maxwang@udel.edu>
2022-10-06 19:17:41 -0400

    initiaed a sig file, DO NOT run this until all functions are working

--- MainShell.c
+++ MainShell.c
@@ -1,4 +1,5 @@
 #include "sh.h"
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,6 +7,7 @@ void sig_hander(int signal);
 
 int main(int argc, char** argv, char** envp){
     //printf("My name is Walter Hartwell  White");
+    //signal(SIGINT,sig_hander);
     return sh(argc,argv,envp);
 }
 
